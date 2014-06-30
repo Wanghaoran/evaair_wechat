22b8f419cfba10e7dc9c5c71ff7353fbab43fdef
WangHaoRan <whrlmc@139.com>
2014-06-30 12:25:13 +0800

    3333

--- index/Lib/Action/IndexAction.class.php
+++ index/Lib/Action/IndexAction.class.php
@@ -19,13 +19,40 @@ class IndexAction extends Action {
 
     public function index(){
 
-        $echoStr = $_GET["echostr"];
+        //get post data, May be due to the different environments
+        $postStr = $GLOBALS["HTTP_RAW_POST_DATA"];
 
-        //valid signature , option
-        if($this->checkSignature()){
-            echo $echoStr;
+        //extract post data
+        if (!empty($postStr)){
+
+            $postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
+            $fromUsername = $postObj->FromUserName;
+            $toUsername = $postObj->ToUserName;
+            $keyword = trim($postObj->Content);
+            $time = time();
+            $textTpl = "<xml>
+							<ToUserName><![CDATA[%s]]></ToUserName>
+							<FromUserName><![CDATA[%s]]></FromUserName>
+							<CreateTime>%s</CreateTime>
+							<MsgType><![CDATA[%s]]></MsgType>
+							<Content><![CDATA[%s]]></Content>
+							<FuncFlag>0</FuncFlag>
+							</xml>";
+            if(!empty( $keyword ))
+            {
+                $msgType = "text";
+                $contentStr = "Welcome to wechat world!";
+                $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
+                echo $resultStr;
+            }else{
+                echo "Input something...";
+            }
+
+        }else {
+            echo "";
             exit;
         }
+
     }
 
         private function checkSignature()
