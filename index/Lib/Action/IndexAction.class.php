a0c70e9b9219d73e40a8c160098127a5048744f7
WangHaoRan <whrlmc@139.com>
2014-06-30 12:40:41 +0800

    333

--- index/Lib/Action/IndexAction.class.php
+++ index/Lib/Action/IndexAction.class.php
@@ -19,63 +19,36 @@ class IndexAction extends Action {
 
     public function index(){
 
+        $echoStr = $_GET["echostr"];
 
-        //get post data, May be due to the different environments
-        $postStr = $GLOBALS["HTTP_RAW_POST_DATA"];
-
-        //extract post data
-        if (!empty($postStr)){
-
-            $postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
-            $fromUsername = $postObj->FromUserName;
-            $toUsername = $postObj->ToUserName;
-            $keyword = trim($postObj->Content);
-            $time = time();
-            $textTpl = "<xml>
-							<ToUserName><![CDATA[%s]]></ToUserName>
-							<FromUserName><![CDATA[%s]]></FromUserName>
-							<CreateTime>%s</CreateTime>
-							<MsgType><![CDATA[%s]]></MsgType>
-							<Content><![CDATA[%s]]></Content>
-							<FuncFlag>0</FuncFlag>
-							</xml>";
-            if(!empty( $keyword ))
-            {
-                $msgType = "text";
-                $contentStr = "Welcome to wechat world!";
-                $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
-                echo $resultStr;
-            }else{
-                echo "Input something...";
-            }
-
-        }else {
-            echo "";
+        //valid signature , option
+        if($this->checkSignature()){
+            echo $echoStr;
             exit;
         }
 
+
+
     }
 
-        private function checkSignature()
-        {
-            $signature = $_GET["signature"];
-            $timestamp = $_GET["timestamp"];
-            $nonce = $_GET["nonce"];
+    private function checkSignature()
+    {
+        $signature = $_GET["signature"];
+        $timestamp = $_GET["timestamp"];
+        $nonce = $_GET["nonce"];
 
-            $token = C('WECHAT_TOKEN');
-            $tmpArr = array($token, $timestamp, $nonce);
-            sort($tmpArr, SORT_STRING);
-            $tmpStr = implode( $tmpArr );
-            $tmpStr = sha1( $tmpStr );
+        $token = C('WECHAT_TOKEN');
+        $tmpArr = array($token, $timestamp, $nonce);
+        sort($tmpArr, SORT_STRING);
+        $tmpStr = implode( $tmpArr );
+        $tmpStr = sha1( $tmpStr );
 
-            if( $tmpStr == $signature ){
-                return true;
-            }else{
-                return false;
-            }
+        if( $tmpStr == $signature ){
+            return true;
+        }else{
+            return false;
         }
-
-
+    }
         /*
                 $content = '长荣航空官方微信建设中...';
 
@@ -110,6 +83,5 @@ class IndexAction extends Action {
         echo $resultStr;
         exit;
         */
-
 
 }
